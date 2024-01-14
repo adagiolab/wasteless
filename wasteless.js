a4b2673e3d78dfb6aa171e016fe446368c38015f
Yunxi <148433028+adagiolab@users.noreply.github.com>
2024-01-14 22:03:50 +0800

    Update wasteless.js
    Updated map script

--- wasteless.js
+++ wasteless.js
@@ -3,26 +3,41 @@
 let map;
 
 async function initMap() {
-  // The location of Uluru
-  const position = { lat: -25.344, lng: 131.031 };
+  // The location of Singapore
+  const position = { lat: 1.290270, lng: 103.851959 };
   // Request needed libraries.
   //@ts-ignore
-  const { Map } = await google.maps.importLibrary("maps");
+  const { Map } = await google.maps.importLibrary('maps');
   const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
 
-  // The map, centered at Uluru
-  map = new Map(document.getElementById("map"), {
+  // The map, centered at Singapore
+  map = new Map(document.getElementById('map'), {
     zoom: 4,
     center: position,
     mapId: "DEMO_MAP_ID",
   });
 
-  // The marker, positioned at Uluru
+  // The marker, positioned at Singapore
   const marker = new AdvancedMarkerElement({
     map: map,
     position: position,
-    title: "Uluru",
+    title: "Singapore",
   });
 }
 
 initMap();
+
+function displayMap() {
+  const mapOptions = {
+    center: { lat: 1.290270, lng: 103.851959 },
+    zoom: 4
+  };
+  const mapDiv = document.getElementById('map');
+  const map = new google.maps.Map(mapDiv, mapOptions);
+  return map;
+}
+
+ loader.then(() => {
+   console.log('Maps JS API loaded');
+   const map = displayMap();
+ });
